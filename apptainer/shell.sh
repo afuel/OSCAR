87ec877af7947837de54c2b0cbcd7f9435517ce2
afuel <audreyfuel33@gmail.com>
2024-06-17 15:29:52 -0400

    Updated build.sif, run.sif, shell.sh, and slurm files

--- apptainer/shell.sh
+++ apptainer/shell.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 base_dir=/work/users/${USER:0:1}/${USER:1:1}/$USER
-apptainer run --nv --bind ${base_dir}/OSCAR:/oscar oscar.sif
+apptainer run --nv --bind ${base_dir}/OSCAR:/oscar run.sif /bin/bash
